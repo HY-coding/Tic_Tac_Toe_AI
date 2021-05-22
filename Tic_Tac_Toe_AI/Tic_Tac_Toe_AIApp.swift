ef2bbed6d5cbaa73d6e38feacc1c000dba6ad7d7
HY <pride508@gmail.com>
2021-05-22 09:24:54 +0800

    Initial Commit

---
+++ Tic_Tac_Toe_AI/Tic_Tac_Toe_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_AIApp.swift
+//  Tic_Tac_Toe_AI
+//
+//  Created by HY Chuang on 2021/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
